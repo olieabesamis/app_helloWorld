899ef2a9a9017ace67d9121e535f708e9b9460f1
Olie.Abesamis <ollieabesamis@gmail.com>
2016-10-26 20:53:03 +0800

    commit #2

--- app_helloWorld/src/main/java/com/pw/olie/main/HelloWorldMain.java
+++ app_helloWorld/src/main/java/com/pw/olie/main/HelloWorldMain.java
@@ -5,6 +5,8 @@ public class HelloWorldMain {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("This is my first class!");
+		
+		System.out.println("This is a message");
 
 	}
 	
