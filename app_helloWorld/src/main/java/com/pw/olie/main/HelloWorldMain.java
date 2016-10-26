57816ed733abfcb6e717564737212ae39124fddd
Olie.Abesamis <ollieabesamis@gmail.com>
2016-10-26 19:54:15 +0800

    Initial Commit

---
+++ app_helloWorld/src/main/java/com/pw/olie/main/HelloWorldMain.java
@@ -0,0 +1,13 @@
+package com.pw.olie.main;
+
+public class HelloWorldMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("This is my first class!");
+
+	}
+	
+	
+
+}
